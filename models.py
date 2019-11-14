26229ab47191f7968909aabaa2b08b096bb5f17a
Anselm Coogan <anselm.coogan@tum.de>
2019-11-14 10:22:32 +0100

    major refactoring

--- models.py
+++ models.py
@@ -6,10 +6,32 @@ from torch.optim.lr_scheduler import MultiStepLR
 
 
 def get_model_by_name(model_name):
-    models = [TwoLayerNN, RandomFourierCluster, RandomFourierReLUCluster]
+    models = [TwoLayerNN, RandomFourier]
     for model in models:
         if model.name() == model_name:
             return model
+
+
+def cross_entropy():
+    return nn.CrossEntropyLoss()
+
+
+def zero_one():
+    pass
+
+
+def mse_loss():
+    return MSELoss()
+
+
+class MSELoss:
+    def __call__(model, outputs, labels):
+        outputs_soft = F.softmax(outputs, dim=0)
+        one_hot_labels = torch.FloatTensor(len(labels), outputs.shape[1])
+        one_hot_labels.zero_()
+        one_hot_labels.scatter_(1, labels.view(-1, 1), 1)
+        loss = torch.mean((outputs - one_hot_labels) ** 2)
+        return loss
 
 
 class CustomModel(nn.Module):
@@ -28,7 +50,7 @@ class CustomModel(nn.Module):
 
     def save(self, file_name):
         torch.save(self.state_dict(), file_name)
-    
+
     def train_step(self, data, device):
         raise NotImplementedError()
 
@@ -42,98 +64,22 @@ class CustomModel(nn.Module):
         pass
 
 
-class RandomFourierReLUCluster:
-    pass
-
-
-class RandomFourierReLUSingle(CustomModel):
-    pass
-
-
-class RandomFourierCluster:
+class RandomFourier(CustomModel):
     def __init__(self, dim_input, num_hidden, dim_output):
-        self._classifiers = []
-        self._train_step = 0
-        self._val_step = 0
-        self._test_step = 0
-        self._num_parameters = num_hidden
-        # one-vs-all
-        for i in range(dim_output):
-            self._classifiers.append(
-                RandomFourierSingle(dim_input, num_hidden, i))
-
-    def save(self, file_name):
-        for clf in self._classifiers:
-            clf.save(file_name + "_" + str(clf.cls))
-
-    def num_parameters(self):
-        return self._num_parameters
-
-    def init_weights(self, prev_model):
-        pass
-
-    def to(self, device):
-        for clf in self._classifiers:
-            clf.to(device)
-
-    def epoch_step(self):
-        self._train_step = 0
-        self._val_step = 0
-
-    def train_step(self, data, device):
-        # train each classifier, return average loss
-        loss = 0.0
-        for clf in self._classifiers:
-            loss += clf.train_step(data, device)
-
-        return loss / len(self._classifiers)
-
-    def val_step(self, data, device):
-        # train each classifier, return average loss
-        loss = 0.0
-        for clf in self._classifiers:
-            loss += clf.train_step(data, device)
-
-        return loss / len(self._classifiers)
-
-    def test_step(self, data, device):
-        # train each classifier, return average loss
-        running_loss = 0.0
-        running_acc = 0.0
-        for clf in self._classifiers:
-            loss, acc = clf.test_step(data, device)
-            running_loss += loss
-            running_acc += acc
-            print("Accuracy: {}".format(acc))
-
-        return running_loss / len(self._classifiers), running_acc / len(self._classifiers)
-
-    @staticmethod
-    def name():
-        return "random_fourier"
-
-
-class RandomFourierSingle(CustomModel):
-    def __init__(self, dim_input, num_hidden, cls):
-        super(RandomFourierSingle, self).__init__()
-        self.hidden = nn.Linear(dim_input, num_hidden)
-        self._init_from_n_dim_unit_sphere(num_hidden, dim_input)
+        super(RandomFourier, self).__init__()
+        self.hidden = nn.Linear(dim_input, num_hidden, bias=False)
+        nn.init.normal_(self.hidden.weight, std=0.04)
         self.hidden.weight.requires_grad = False  # fix weights
-        self.out = nn.Linear(num_hidden, 1)
+        self.out = nn.Linear(num_hidden, dim_output, bias=False)
         self._optimizer = optimizers.SGD(
             self.parameters(), lr=0.001, momentum=0.95)
-        self.cls = cls
         self._loss_function = nn.MSELoss()
-
-    def _init_from_n_dim_unit_sphere(self, num_points, dim):
-        with torch.no_grad():
-            weights = torch.randn(dim, num_points)
-            weights /= torch.norm(weights, dim=0)
-            self.hidden.weight = nn.Parameter(weights.T)
+        self._one_hot_label = None
+        self._dim_output = dim_output
 
     def forward(self, x):
         x = torch.cos(self.hidden(x))  # exp(i * x) = cos(x) (+ i*sin(x))
-        x = torch.sigmoid(self.out(x))
+        x = nn.Softmax(self.out(x))
         return x
 
     def train_step(self, data, device):
@@ -142,11 +88,12 @@ class RandomFourierSingle(CustomModel):
             self.train()
         inputs, labels = data[0].to(
             device), data[1].to(device)
-        labels = (labels == self.cls).float()
-        self._optimizer.zero_grad()
         outputs = self(inputs)
-        loss = self._loss_function(outputs.view(-1), labels) + \
-            self.out.weight.norm(2)  # + l2_reg for a_i
+        one_hot_labels = torch.FloatTensor(len(labels), self._dim_output)
+        one_hot_labels.zero_()
+        one_hot_labels.scatter_(1, labels.view(-1, 1), 1)
+        self._optimizer.zero_grad()
+        loss = self._loss_function(outputs.dim, one_hot_labels)
         loss.backward()
         self._optimizer.step()
         return loss.item()
@@ -154,9 +101,11 @@ class RandomFourierSingle(CustomModel):
     def val_step(self, data, device):
         inputs, labels = data[0].to(
             device), data[1].to(device)
-        labels = (labels == self.cls).float()
         outputs = self(inputs)
-        loss = self._loss_function(outputs.view(-1), labels) + self.out.weight.norm(2)
+        one_hot_labels = torch.FloatTensor(len(labels), self._dim_output)
+        one_hot_labels.zero_()
+        one_hot_labels.scatter_(1, labels.view(-1, 1), 1)
+        loss = self._loss_function(outputs.dim, one_hot_labels)
         return loss.item()
 
     def test_step(self, data, device):
@@ -165,16 +114,21 @@ class RandomFourierSingle(CustomModel):
             self.eval()
         inputs, labels = data[0].to(
             device), data[1].to(device)
-        labels = (labels == self.cls).float()
         outputs = self(inputs)
-        loss = self._loss_function(outputs.view(-1), labels)
+        one_hot_labels = torch.FloatTensor(len(labels), self._dim_output)
+        one_hot_labels.zero_()
+        one_hot_labels.scatter_(1, labels.view(-1, 1), 1)
+        loss = self._loss_function(outputs.dim, one_hot_labels)
         test_loss = loss.item()
-        predicted = torch.round(outputs)
+        _, predicted = torch.max(outputs.dim, 1)
         total = labels.size(0)
         correct = (predicted == labels).sum().item()
         accuracy = correct/total
+        return test_loss, accuracy
 
-        return test_loss, accuracy, outputs
+    @staticmethod
+    def name():
+        return "random_fourier"
 
 
 class TwoLayerNN(CustomModel):
@@ -182,8 +136,9 @@ class TwoLayerNN(CustomModel):
         super(TwoLayerNN, self).__init__()
         self.hidden = nn.Linear(dim_input, num_hidden)
         self.out = nn.Linear(num_hidden, dim_output)
-        self._loss_function = TwoLayerNN.mse_loss
-        self._loss_function = nn.CrossEntropyLoss()
+        self._dim_output = dim_output
+        self._subset_size = 4000
+        self._loss_function = loss_fn
         self._optimizer = optimizers.SGD(
             self.parameters(), lr=0.001, momentum=0.95)
         self.scheduler = MultiStepLR(self._optimizer, milestones=list(
@@ -195,7 +150,7 @@ class TwoLayerNN(CustomModel):
         return x
 
     def init_weights(self, prev_model):
-        if prev_model is None:
+        if prev_model is None or self.num_parameters() > self._subset_size * self._dim_output:
             # initialize smallest net using Xavier Glorot-uniform distribution
             gain = nn.init.calculate_gain("relu")
             nn.init.xavier_uniform_(self.hidden.weight, gain=gain)
@@ -231,28 +186,6 @@ class TwoLayerNN(CustomModel):
         loss = self._loss_function(outputs, labels)
         return loss.item()
 
-    @staticmethod
-    def get_loss_fn_by_name(name):
-        losses = {"cross_entropy": nn.CrossEntropyLoss,
-                  "squared": TwoLayerNN.mse_loss,
-                  "zero_one": TwoLayerNN.zero_one_loss}
-        return losses.get(name)
-
-    @staticmethod
-    def zero_one_loss(output, labels):
-        pass
-
-    @staticmethod
-    def mse_loss(outputs, labels):
-        print(outputs)
-        indices = [(col, int(row)) for col, row in enumerate(labels)]
-        outputs = torch.tensor(
-            [outputs[elem] for elem in indices], dtype=torch.float, requires_grad=True)
-        ones = torch.ones(outputs.shape)
-        # print(outputs)
-        loss = torch.mean((outputs - ones) ** 2)
-        return loss
-
     def test_step(self, data, device):
         self._test_step += 1
         if self._test_step == 1:
@@ -262,7 +195,7 @@ class TwoLayerNN(CustomModel):
         outputs = self(inputs)
         loss = self._loss_function(outputs, labels)
         test_loss = loss.item()
-        predicted, _ = torch.max(outputs.data, 1)
+        _, predicted = torch.max(outputs.data, 1)
         total = labels.size(0)
         correct = (predicted == labels).sum().item()
         accuracy = correct/total
