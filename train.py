4871f58aee3f9bbfc815fc684d7b18355dab3377
Anselm Coogan <anselm.coogan@tum.de>
2019-11-14 10:22:57 +0100

    adjusted and added cli args and some refactoring

--- train.py
+++ train.py
@@ -8,14 +8,20 @@ import argparse
 import torch
 
 # own
-from models import get_model_by_name
+from models import get_model_by_name, cross_entropy, mse_loss, zero_one
 from helpers import get_dataset, Progress
 
 AVAILABLE_MODELS = ["two_layer_nn", "random_fourier"]
 
+AVAILABLE_LOSS_FNS = [
+    mse_loss(),
+    cross_entropy(),
+    zero_one()
+]
+
 
 class Training:
-    def __init__(self, model, dataset, num_params, epochs, prev_model=None, num_models=None):
+    def __init__(self, model, dataset, num_params, epochs, loss_fn, prev_model=None, num_models=None):
         self._train_loader = dataset.train_loader
         self._val_loader = dataset.val_loader
         self._test_loader = dataset.test_loader
@@ -26,6 +32,7 @@ class Training:
             "cuda:0" if torch.cuda.is_available() else "cpu")
         self._all_models = []
         self._epochs = epochs
+        self._loss_name = type(loss_fn).__name__
         self.all_train_losses = {}
         self.all_val_losses = {}
         self.all_test_losses = {}
@@ -34,7 +41,8 @@ class Training:
         input_dim = self._val_loader.dataset[0][0].size()[0]
         if prev_model is not None:
             prev_num_hidden = len(prev_model['hidden.weight'])
-            self._prev_model = model(input_dim, prev_num_hidden, num_classes)
+            self._prev_model = model(
+                input_dim, prev_num_hidden, num_classes, loss_fn)
             self._prev_model.load_state_dict(prev_model)
             num_params = num_params[num_params.index(prev_num_hidden) + 1:]
         else:
@@ -42,21 +50,29 @@ class Training:
 
         if num_models is not None:
             num_params = num_params[:num_models]
-        
+
         for p in num_params:
-            self._all_models.append(model(input_dim, p, num_classes))
+            self._all_models.append(model(input_dim, p, num_classes, loss_fn))
 
     def save(self):
-        path = os.path.join("data", "results", "epochs_" + str(self._epochs))
-        file_name = os.path.join(path, self._model_name + ".json")
+        path = os.path.join("data", "results",
+                            self._model_name, self._loss_name)
+
+        file_name = os.path.join(path, str(self._epochs) + ".json")
         if not os.path.exists(path):
             os.makedirs(path)
         content = {}
         content["Train losses"] = self.all_train_losses
         content["Val losses"] = self.all_val_losses
         content["Test losses"] = self.all_test_losses
+        if os.path.exists(file_name):
+            with open(file_name, "r") as fd:
+                old_content = json.load(fd)
+            for loss_key in old_content.keys():
+                for model_key in old_content[loss_key].keys():
+                    content[loss_key][model_key] = old_content[loss_key][model_key]
         with open(file_name, "w") as fd:
-            json.dump(content, fd)
+            json.dump(content, fd, indent=4, sort_keys=True)
 
     def start(self):
         try:
@@ -106,14 +122,14 @@ class Training:
             progress.finished_model(
                 model.num_parameters(), test_loss, accuracy)
 
-            model_name = self._model_name + "_" + str(model.num_parameters())
+            model_name = str(model.num_parameters())
             self.all_test_losses[model_name] = test_loss
             self.all_train_losses[model_name] = train_losses
             self.all_val_losses[model_name] = val_losses
             self._prev_model = model
 
             path = os.path.join(
-                "data", "models", self._model_name, "epochs_" + str(self._epochs))
+                "data", "models", self._model_name, self._loss_name, "epochs_" + str(self._epochs))
             if not os.path.exists(path):
                 os.makedirs(path)
             file_name = os.path.join(path, model_name)
@@ -139,6 +155,12 @@ if __name__ == "__main__":
     parser.add_argument(
         "--config", type=str, help="config file for training", required=True, dest="config")
 
+    parser.add_argument(
+        "--loss", type=int, help="The loss function to use 0 [Squared](default), 1 [Zero-one], 2 [Cross-Entropy]", dest="loss", default=0, choices={0, 1, 2})
+
+    parser.add_argument(
+        "--epochs", type=int, help="The number of epochs to train, if not given, the value in the config file is taken", dest="epochs")
+
     args = parser.parse_args()
 
     with open(args.config, "r") as fd:
@@ -146,11 +168,14 @@ if __name__ == "__main__":
 
     dataset_name = "mnist"
     model_name = AVAILABLE_MODELS[args.model]
+    loss_fn = AVAILABLE_LOSS_FNS[args.loss]
     batch_size = config["batch_size"][dataset_name.lower()]
     train_subset_size = config["train_subset_size"][model_name.lower()]
-    epochs = config["epochs"]
+    epochs = args.epochs if args.epochs else config["epochs"]
     previous_model = None
     if args.prev:
+        if type(loss_fn).__name__ not in args.prev:
+            raise RuntimeError("Use same loss function")
         if not os.path.exists(args.prev):
             raise FileNotFoundError("Couldn't find {}".format(args.prev))
         previous_model = torch.load(args.prev)
@@ -162,6 +187,6 @@ if __name__ == "__main__":
     dataset = get_dataset(dataset_name, train_subset_size, batch_size)
 
     training = Training(model, dataset, num_params,
-                        epochs, prev_model=previous_model, num_models=num_models)
+                        epochs, prev_model=previous_model, num_models=num_models, loss_fn=loss_fn)
     training.start()
     training.save()
