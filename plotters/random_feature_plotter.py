b7a9a3c89050c5faf15ff4b8fdab7deedc32fecf
Anselm Coogan <anselm.coogan@tum.de>
2019-12-04 10:05:01 +0100

    unified plotting scripts

---
+++ plotters/random_feature_plotter.py
@@ -0,0 +1,121 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import numpy as np
+import json
+import os
+import argparse
+
+
+def plot_results(results, file_prefix, label_color, background_color, transparent):
+    num_params = 2 * np.array(results.pop("num_parameters"))
+    norms = np.array(results.pop("norms"))
+    titles = [x.replace("_", " ") for x in results.keys()]
+    fig = plt.figure(figsize=(9, 6))
+    ax1 = plt.subplot(221)
+    ax1.set_xticklabels([])
+    ax1.set_ylabel("Loss", color=label_color, size=9)
+    ax2 = plt.subplot(222)
+    ax2.set_ylabel("Loss in %", color=label_color, size=9)
+    ax2.set_xticklabels([])
+    ax3 = plt.subplot(223)
+    ax3.set_ylabel("Loss", color=label_color, size=9)
+    ax3.set_xlabel("N", color=label_color, size=9)
+    ax4 = plt.subplot(224)
+    ax4.set_ylabel("Loss in %", color=label_color, size=9)
+    ax4.set_xlabel("N", color=label_color, size=9)
+    axes = [ax1, ax2, ax3, ax4]
+    for i, ax in enumerate(axes):
+        key = list(results.keys())[i]
+        data = np.array(results[key])
+        if "zero" in titles[i]:
+            data *= 100  # show as percentage
+        if "test" in titles[i]:
+            if "zero" in titles[i]:
+                ax.hlines(3.9, xmin=0, xmax=max(num_params),
+                          color="C0", label="min. norm kernel")
+            else:
+                ax.hlines(0.13, xmin=0, xmax=max(num_params),
+                          color="C0", label="min. norm kernel")
+
+            ax.semilogy(num_params, data, ".-", color="orange", label="RFF")
+        else:
+            ax.plot(num_params, data, ".-", color="orange", label="RFF")
+        ax.vlines(10000, ymin=0, ymax=max(data), color=label_color,
+                  linestyle="dotted", label="interpolation")
+        if "zero" in titles[i]:
+            ax.yaxis.set_major_formatter(mticker.ScalarFormatter())
+            ax.yaxis.get_major_formatter().set_scientific(False)
+            ax.yaxis.get_major_formatter().set_useOffset(False)
+        ax.set_yticks([], minor=True)
+        ax.set_title(titles[i].upper(), size=9, color=label_color)
+        ax.set_facecolor(background_color)
+        ax.tick_params(colors=label_color, which="both")
+        ax.spines["top"].set_visible(False)
+        ax.spines["bottom"].set_color(label_color)
+        ax.spines["right"].set_visible(False)
+        ax.spines["left"].set_color(label_color)
+        legend = ax.legend(frameon=False, fontsize=9)
+        plt.setp(legend.get_texts(), color="w")
+    fig.tight_layout()
+    fig.set_facecolor(background_color)
+    plt.savefig(file_prefix + "_losses.pdf", transparent=transparent)
+    plt.show()
+
+    norm_fig = plt.figure(figsize=(9, 6))
+    ax = plt.subplot()
+
+    try:
+        ax.semilogy(num_params, norms.mean(axis=1), ".-", color="orange")
+    except:
+        ax.semilogy(num_params, norms, ".-", color="orange", label="RFF")
+    ax.hlines(8, xmin=0, xmax=max(num_params),
+              color="C0", label="min. norm kernel")
+    ax.spines["top"].set_visible(False)
+    ax.spines["bottom"].set_color(label_color)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_color(label_color)
+    ax.set_facecolor(background_color)
+    ax.set_ylabel("L2 norm", color=label_color)
+    ax.set_xlabel("N", color=label_color)
+    ax.tick_params(colors=label_color, which="both")
+    ax.set_yticks([], minor=True)
+    legend = ax.legend(frameon=False)
+    plt.setp(legend.get_texts(), color="w")
+    norm_fig.set_facecolor(background_color)
+
+    norm_fig.tight_layout()
+    plt.savefig(file_prefix + "_norms.pdf", transparent=True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Plot random feature results from experiments")
+
+    parser.add_argument("results", type=argparse.FileType('r'),
+                        nargs='+', help="the result json file(s). Expected in order of increasing capacity sizes")
+
+    parser.add_argument("--saveto", dest="file_prefix", type=str,
+                        help="The file prefix to save the plots to", required=True)
+
+    parser.add_argument("--bg", det="bg", type=str,
+                        help="The desired background color of the plot. Any acceptable matplotlib string (default \"black\")", default="black")
+    parser.add_argument("--fg", det="fg", type=str,
+                        help="The desired label color of the plot. Any acceptable matplotlib string (default \"white\")", default="white")
+    parser.add_argument("--transparent", action="store_true",
+                        help="Save plot with transparent background", default=False)
+
+    args = parser.parse_args()
+    transparent = parser.transparent
+    bg = parser.bg
+    fg = parser.fg
+    results = {}
+    for result in args.results:
+        result = json.load(result)
+        for k, v in result.items():
+            if k in results:
+                results[k] += v
+            else:
+                results[k] = v
+
+    plot_results(results, args.file_prefix, fg, bg, transparent)
--- plotters/two_layer_plotter.py
+++ plotters/two_layer_plotter.py
@@ -3,57 +3,55 @@ from collections import OrderedDict
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 
-test_file = "data/results/two_layer_nn/CrossEntropyLoss/1000.json"
-test_model = "two_layer_nn_3985"
-def plot_double_descent(file_name):
-    with open(file_name, "r") as fd:
+
+def plot_double_descent(result_file, file_prefix, label_color, background_color):
+    with open(result_file, "r") as fd:
         content = json.load(fd)
 
-    sizes = sorted([int(x.split("_")[-1])/1e3 for x in content["Train losses"].keys()])
-    train_losses = OrderedDict(sorted(content["Train losses"].items(), key= lambda t: int(t[0].split("_")[-1])))
-    val_losses = OrderedDict(sorted(content["Val losses"].items(), key= lambda t: int(t[0].split("_")[-1])))
+    sizes = sorted(
+        [int(x.split("_")[-1])/1e3 for x in content["Train losses"].keys()])
+    train_losses = OrderedDict(
+        sorted(content["Train losses"].items(), key=lambda t: int(t[0].split("_")[-1])))
+    val_losses = OrderedDict(
+        sorted(content["Val losses"].items(), key=lambda t: int(t[0].split("_")[-1])))
     final_train_losses = [v[-1] for v in train_losses.values()]
     final_val_losses = [v[-1] for v in val_losses.values()]
     test_losses = list(content["Test losses"].values())
-    test_losses = list(OrderedDict(sorted(content["Test losses"].items(), key= lambda t: int(t[0].split("_")[-1]))).values())
+    test_losses = list(OrderedDict(sorted(content["Test losses"].items(
+    ), key=lambda t: int(t[0].split("_")[-1]))).values())
     fig, ax = plt.subplots()
-    ax.set_facecolor("black")
+    ax.set_facecolor(background_color)
     ax.semilogx(sizes, final_train_losses, ".-", label="Train", color="C0")
     ax.semilogx(sizes, test_losses, ".-", label="Test", color="C1")
     ax.set_xticks([], minor=True)
     ax.set_xticks([10, 40, 100, 300, 800], minor=False)
-    ax.vlines(40e3/1e3, ymin=0, ymax=max(test_losses), linestyle="dotted" , color="white", label="Interpolation")
-    ax.tick_params(colors="white", which="both")
+    ax.vlines(40e3/1e3, ymin=0, ymax=max(test_losses),
+              linestyle="dotted", color=label_color, label="Interpolation")
+    ax.tick_params(colors=label_color, which="both")
     ax.spines["top"].set_visible(False)
-    ax.spines["bottom"].set_color("white")
-    ax.spines["right"].set_visible(False) 
-    ax.spines["left"].set_color("white")
+    ax.spines["bottom"].set_color(label_color)
+    ax.spines["right"].set_visible(False)
+    ax.spines["left"].set_color(label_color)
     ax.xaxis.set_major_formatter(mticker.ScalarFormatter())
     ax.xaxis.get_major_formatter().set_scientific(False)
     ax.xaxis.get_major_formatter().set_useOffset(False)
     legend = ax.legend(frameon=False, fontsize=9)
     plt.setp(legend.get_texts(), color="w")
-    plt.ylabel("Loss", color="white")
-    plt.xlabel(r"N $(\times 10^3)$", color="white")
-    plt.savefig("../presentation/resources/two_layer_nn.pdf", transparent=True)
+    plt.ylabel("Loss", color=label_color)
+    plt.xlabel(r"N $(\times 10^3)$", color=label_color)
+    plt.savefig(file_prefix + "two_layer_double_descent.pdf",
+                transparent=transparent)
     plt.show()
-    
+
+
 def plot_training(file_name):
     with open(file_name, "r") as fd:
         content = json.load(fd)
 
     for model_name in content["Train losses"].keys():
         plot_single_model(content, model_name)
-    
-def p():
-    plot_double_descent(test_file)
 
-def ps():
-    plot_single_model(test_file, test_model)
 
-def pt():
-    plot_training(test_file)
-    
 def plot_single_model(content, model_name):
     train_losses = content["Train losses"][model_name]
     val_losses = content["Val losses"][model_name]
@@ -62,6 +60,27 @@ def plot_single_model(content, model_name):
     plt.plot(val_losses, label="Val")
     plt.title(model_name)
     plt.show()
-    
-if __name__=="__main__":
-    p()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Plot two layer neural net results from experiments")
+
+    parser.add_argument("result", type=argparse.FileType('r'),
+                        help="the result json file.")
+    parser.add_argument("--saveto", dest="file_prefix", type=str,
+                        help="The file prefix to save the plots to", required=True)
+
+    parser.add_argument("--bg", det="bg", type=str,
+                        help="The desired background color of the plot. Any acceptable matplotlib string (default \"black\")", default="black")
+    parser.add_argument("--fg", det="fg", type=str,
+                        help="The desired label color of the plot. Any acceptable matplotlib string (default \"white\")", default="white")
+    parser.add_argument("--transparent", action="store_true",
+                        help="Save plot with transparent background", default=False)
+
+    args = parser.parse_args()
+    transparent = parser.transparent
+    bg = parser.bg
+    fg = parser.fg
+    result = args.result
+    plot_double_descent(results, args.file_prefix, fg, bg)
